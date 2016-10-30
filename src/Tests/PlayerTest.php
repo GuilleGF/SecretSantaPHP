a9ab6c2f0526fbbc900d7f6097e14377390aec65
GuilleGF <guillermogf@gmail.com>
2016-10-30 23:38:39 +0100

    Add test player

---
+++ src/Tests/PlayerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use SecretSanta\Player;
+
+/**
+ * Class PlayerTest
+ */
+class PlayerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \SecretSanta\Exceptions\PlayerException
+     */
+    public function testSortName()
+    {
+        Player::create('ab', 'email@email.com');
+    }
+
+    /**
+     * @expectedException \SecretSanta\Exceptions\PlayerException
+     */
+    public function testInvalidEmail()
+    {
+        Player::create('name', 'email@');
+    }
+
+    public function testPlayer()
+    {
+        $player = Player::create('name', 'email@email.com');
+
+        $this->assertSame('name', $player->name());
+        $this->assertSame('email@email.com', $player->email());
+    }
+}
